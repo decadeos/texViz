87fb0c859bbc94177a40759d8bca70b968542bb5
decadeos <ohrimenkoed@gmail.com>
2025-03-07 23:17:19 +0300

    optimise by show, size, ets and done reflect()

--- lab2/code.py
+++ lab2/code.py
@@ -2,11 +2,9 @@ import cv2
 import matplotlib.pyplot as plt
 from defs import *
 
-
 ## Image
 I = cv2.imread("lab2/image.jpg")
 I_rgb = convert_to_rgb(I)
-
 
 ## param sdvig
 tx = 50
@@ -14,19 +12,18 @@ ty = 100
 I_shift = sdvig(I, tx, ty)
 I_shift_rgb = convert_to_rgb(I_shift)
 
-
+## param reflect
+I_reflect = reflect(I)
+I_reflect_rgb = convert_to_rgb(I_reflect)
 
 
 
 
 ## show image
-plt.imshow(I_rgb)
-plt.title("Original Image")
-plt.axis('off')
-plt.show()
+show(I_rgb, "original image")
 
 ## show sdvig image
-plt.imshow(I_shift_rgb)
-plt.title("Shifted Image")
-plt.axis('off')
-plt.show()
+show(I_shift_rgb, "shifted image")
+
+## show reflect image
+show(I_reflect_rgb, "reflected image")
--- lab2/defs.py
+++ lab2/defs.py
@@ -2,11 +2,28 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sdvig(image, tx, ty):
+def show(image, title):
+    plt.imshow(image)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+def convert_to_rgb(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def size(image):
     rows, cols = image.shape[0:2]
+    return rows, cols
+
+def sdvig(image, tx, ty):
+    rows, cols = size(image)
     T = np.float32([[1, 0, tx], [0, 1, ty]])
     shifted_image = cv2.warpAffine(image, T, (cols, rows))
     return shifted_image
 
-def convert_to_rgb(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def reflect(image):
+    rows, cols = size(image)
+    T = np.float32([[1 , 0 , 0] ,[0, -1, rows - 1]])
+    I_reflect = cv2.warpAffine(image, T, (cols, rows))
+    return I_reflect
