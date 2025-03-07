0115b4f23909cc14e46bebe1ac5e91ea4a886ad7
decadeos <ohrimenkoed@gmail.com>
2025-03-07 23:42:22 +0300

    done scaling (it's funny)

--- lab2/code.py
+++ lab2/code.py
@@ -12,9 +12,30 @@ ty = 100
 I_shift = sdvig(I, tx, ty)
 I_shift_rgb = convert_to_rgb(I_shift)
 
-## param reflect
-I_reflect = reflect(I)
-I_reflect_rgb = convert_to_rgb(I_reflect)
+## param reflect OX
+I_reflect_OX = reflectOX(I)
+I_reflect_OX_rgb = convert_to_rgb(I_reflect_OX)
+
+## param reflect OY
+I_reflect_OY = reflectOY(I)
+I_reflect_OY_rgb = convert_to_rgb(I_reflect_OY)
+
+## param scaling
+scale_x = 1.7
+scale_y = 0.5
+I_scale = scaling(I, scale_x, scale_y)
+I_scale_rgb = convert_to_rgb(I_scale)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -25,5 +46,11 @@ show(I_rgb, "original image")
 ## show sdvig image
 show(I_shift_rgb, "shifted image")
 
-## show reflect image
-show(I_reflect_rgb, "reflected image")
+## show reflect by OX image
+show(I_reflect_OX_rgb, "reflected by OX image")
+
+## show reflect by OY image
+show(I_reflect_OY_rgb, "reflected by OY image")
+
+## show scaled image
+show(I_scale_rgb, "scaled image")
--- lab2/defs.py
+++ lab2/defs.py
@@ -21,9 +21,20 @@ def sdvig(image, tx, ty):
     shifted_image = cv2.warpAffine(image, T, (cols, rows))
     return shifted_image
 
-
-def reflect(image):
+def reflectOX(image):
     rows, cols = size(image)
     T = np.float32([[1 , 0 , 0] ,[0, -1, rows - 1]])
     I_reflect = cv2.warpAffine(image, T, (cols, rows))
     return I_reflect
+
+def reflectOY(image):
+    rows, cols = size(image)
+    T = np.float32([[-1, 0, cols - 1], [0, 1, 0]])
+    I_reflect = cv2.warpAffine(image, T, (cols, rows))
+    return I_reflect
+
+def scaling(image, scale_x, scale_y):
+    rows, cols = size(image)
+    T = np.float32([[scale_x, 0, 0] ,[0, scale_y, 0]])
+    I_scale = cv2.warpAffine(image, T, (int(cols * scale_x), int(rows*scale_y)))
+    return I_scale
