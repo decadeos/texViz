5846f6957e0b3eddf053744591d50fbf329916e1
decadeos <ohrimenkoed@gmail.com>
2025-03-08 19:20:11 +0300

    task2 doooone!

--- lab2/task2/code.py
+++ lab2/task2/code.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 from defs import *
 import os
 
-# Путь к файлу
-image_path = os.path.join(os.path.dirname(__file__), "podushka.png")
-I = cv2.imread(image_path)
-I_rgb = convert_to_rgb(I)
+# Путь к файлу 1
+image_path1 = os.path.join(os.path.dirname(__file__), "podushka.png")
+I1 = cv2.imread(image_path1)
+I_rgb_1 = convert_to_rgb(I1)
 
-I_barrel = correct_distortion(I_rgb, 0.1, 0.12, "barrel")
-I_pincushion = correct_distortion(I_rgb, 0.1, 0.12, "pincushion")
+# Путь к файлу 2
+image_path2 = os.path.join(os.path.dirname(__file__), "bochka.png")
+I2 = cv2.imread(image_path2)
+I_rgb_2 = convert_to_rgb(I2)
 
-show(I_rgb, "Original Image")
+I_barrel = correct_distortion(I_rgb_1, 0.1, 0.12, "barrel")
+I_pincushion = correct_distortion(I_rgb_2, 0.1, 0.12, "pincushion")
+
 show(I_barrel, "Barrel Distortion Correction")
 show(I_pincushion, "Pincushion Distortion Correction")
